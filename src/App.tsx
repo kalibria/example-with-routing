a918dbfa55ea429cab04ee8e319bcdbc627c4690
Maria Kazakova <mariakazakova92@gmail.com>
2024-07-17 15:11:36 +0300

    added NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styles from "./components/Site.module.css";
 import {PageOne} from "./components/pages/PageOne";
 import {PageTwo} from "./components/pages/PageTwo";
 import {PageThree} from "./components/pages/PageThree";
-import {Navigate, Route, Routes} from 'react-router-dom';
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
 import {Error404} from "./components/pages/Error404";
 
 
@@ -13,7 +13,9 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    Здесь будет навигация
+                    <div><NavLink to={'/page1'}>Page 1</NavLink></div>
+                    <div><NavLink to={'/page2'}>Page 2</NavLink></div>
+                    <div><NavLink to={'/page3'}>Page 3</NavLink></div>
                 </div>
                 <div className={styles.content}>
                     <Routes>
